0b2d6b1f0dc9ae04b5d4f5b38c3bd81acad90833
liamhorton0528 <115037634+liamhorton0528@users.noreply.github.com>
2023-11-07 17:37:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 3001;
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const {body, validationResult} = require('express-validator');
 
 app.use(express.urlencoded());
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
-
 app.use((req,res,next) =>{
 console.log(`${req.method} request for ${req.url}`);
 next();
@@ -41,24 +41,6 @@ app.get('/', (req, res) => {
               </div>`);
 });
 
-//creating user page
-/* app.get('/api/users', async (req, res) => {
-    try {
-        const delay = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(users);
-            }, 1500);
-        })
-
-        const result = await delay;
-        res.render('displayUsers.ejs', {users:result});
-        processNumOfUsers(showNumOfUsers);
-    }catch(error) {
-        console.log(error);
-        res.status(500).send(`${error}`);
-    }
-}) */
-
 app.get('/api/users', async (req, res) => {
     try{
         const users = await User.find();
@@ -69,35 +51,12 @@ app.get('/api/users', async (req, res) => {
     }
 })
 
-function showNumOfUsers(num) {
-    console.log('There are ' + num + ' listed users.');
-}
-
-function processNumOfUsers(callback) {
-    const num = users.length;
-    callback(num);
-}
-
-
-//add operations
-app.get('/api/users/add', async (req, res) => {
-    try {
-        const delay = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(users);
-            }, 1500);
-        })
-
-        const result = await delay;
-        res.render('addUsers.ejs', {users:result});
-    }catch(error) {
-        console.log(error);
-        res.status(500).send(`${error}`);
-    }
-})
-
-app.post('/api/users', async (req, res)=>{
+app.post('/api/users', [body('name').isLength({min: 1})], async (req, res) => {
     const newUser = new User({name: req.body.name});
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
 
     try{
         const result = await newUser.save();
@@ -108,6 +67,40 @@ app.post('/api/users', async (req, res)=>{
         res.status(500).send('Error saving to database');
     }
 })
+
+//update
+app.post('/api/users/update', [body('newName').isLength({min: 1})], async (req, res) => {
+    const userID = req.body.id;
+    const newName = req.body.newName;
+    console.log(userID + ' ' + newName);
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const doc = await User.findById(userID);
+    doc.name = newName;
+    await doc.save();
+    res.redirect('/api/users');
+})
+
+//delete
+app.post('/api/users/delete', async (req, res) => {
+    const userID = req.body.id;
+    const doc = await User.findById(userID);
+    await doc.deleteOne();
+    res.redirect('/api/users');
+})
+
+function showNumOfUsers(num) {
+    console.log('There are ' + num + ' listed users.');
+}
+
+function processNumOfUsers(callback) {
+    const num = users.length;
+    callback(num);
+}
 
 
 //creating server
